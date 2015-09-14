b1f090821d204c02e830fcdd231fc5a893ac30f2
Leonardo Navarro <maikndawer@gmail.com>
2015-09-14 18:50:34 -0300

    otgqlksdfjanpñeirwlnberM

--- config/config_app.php
+++ config/config_app.php
@@ -4,7 +4,7 @@ class ConfigApp {
 
   public static $ACTION = 'action';
   public static $ACTION_DEFAULT = 'home';
-  public static $ACTION_AGREGAR_TAREA = 'agregar_tarea';
-  public static $ACTION_BORRAR_TAREA = 'borrar_tarea';
+  public static $ACTION_CATALOG = 'catalog';
+  public static $ACTION_FAQS = 'faqs';
 }
- ?>
+
