f7138275093ae45804e2dc5a450a6d87a6cbe568
vickydr04 <vickydr04@gmail.com>
2015-09-19 14:05:32 -0300

    changeeee

--- view/MainView.php
+++ view/MainView.php
@@ -2,40 +2,41 @@
 include_once 'libs/Smarty.class.php';
 
 class MainView{
-	
+
 	private $smarty;
     private $errors;
  	private $tpl_dir = 'templates/';
     private $template = 'templates/index.tpl';
     private $component = 'home';
-    
+
 	function __construct(){
 		$this->smarty = new Smarty();
 		$this->errors = array();
-		
+
 		$this->smarty->caching = false;
 	}
-	
+
 	function show($component = null){
 		$this->smarty->assign('errors', $this->errors);
 		if ($component == null)
 			$this->smarty->assign('component', $component);
-		else 
+		else
 			$this->smarty->assign('component', $this->tpl_dir . $this->component. '.tpl');
-		
+
 		$this->smarty->display($this->template);
 	}
-	
+
 	function getHTML($component, $middlePath = null){
 		if ($middlePath == null)
 			$path = $this->tpl_dir . $component . '.tpl';
-		else 
+		else
 			$path = $this->tpl_dir . $middlePath . $component . '.tpl';
-			 
+
 		return $this->smarty->fetch($path);
 	}
-	
+
 	function showErrors($error){
 		array_push($this->errors, $error);
 	}
+
 }
