5d2cf9a22594f71189e8192ce0eeb794f2b82afe
vickydr04 <vickydr04@gmail.com>
2015-09-19 15:01:46 -0300

    baseee

--- model/BaseModel.php
+++ model/BaseModel.php
@@ -1,7 +1,7 @@
 <?php
 class BaseModel {
 
-  private $tareas;
+  private $book;
   private $db;
 
   function __construct() {
@@ -9,6 +9,22 @@ class BaseModel {
       $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
 
+  function uploadBook($archivo){
+    $destino = 'uploaded/books/'.uniqid().$archivo['name'];
+    move_uploaded_file($archivo['tmp_name'], $destino);
+    return $destino;
+  }
+
+  function saveBook($book, $file){
+    $ruta = $this->uploadBook($file);
+    $consulta = $this->db->prepare('INSERT INTO libro(nombre_libro, descripcion_libro, url_libro, seccion_id_seccion)
+                                    VALUES(:name, :description, :url, :section)');
+    $consulta->bindParam(':name', $book->name);
+    $consulta->bindParam(':description', $book->description);
+    $consulta->bindParam(':url', $book->url);
+    $consulta->bindParam(':section', $book->section);
+    $consulta->execute();
+  }
 
 }
 ?>
