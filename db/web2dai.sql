eec73c6054b33041f1892befe6a1f683d9f69b31
vickydr04 <vickydr04@gmail.com>
2015-09-28 16:48:33 -0300

    base

--- db/web2dai.sql
+++ db/web2dai.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 17-09-2015 a las 20:11:10
--- Versión del servidor: 5.5.44-0ubuntu0.14.04.1
--- Versión de PHP: 5.5.9-1ubuntu4.11
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-09-2015 a las 21:46:05
+-- Versión del servidor: 5.6.25
+-- Versión de PHP: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `web2dai`
@@ -27,10 +27,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `categoria` (
-  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre_categoria` varchar(200) NOT NULL,
-  PRIMARY KEY (`id_categoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id_categoria` int(11) NOT NULL,
+  `nombre_categoria` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -39,13 +38,10 @@ CREATE TABLE IF NOT EXISTS `categoria` (
 --
 
 CREATE TABLE IF NOT EXISTS `categoria_libro` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `id_fk_libro` int(11) NOT NULL,
-  `id_fk_categoria` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_fk_libro` (`id_fk_libro`),
-  KEY `id_fk_categoria` (`id_fk_categoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id_fk_categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -54,14 +50,21 @@ CREATE TABLE IF NOT EXISTS `categoria_libro` (
 --
 
 CREATE TABLE IF NOT EXISTS `libro` (
-  `id_libro` int(11) NOT NULL AUTO_INCREMENT,
+  `id_libro` int(11) NOT NULL,
   `nombre_libro` varchar(200) NOT NULL,
-  `descripcion_libro` varchar(400) NOT NULL,
+  `autor_libro` varchar(200) NOT NULL,
+  `img_libro` varchar(300) NOT NULL,
   `url_libro` varchar(200) NOT NULL,
-  `seccion_id_seccion` int(11) NOT NULL,
-  PRIMARY KEY (`id_libro`),
-  KEY `fk_libro_seccion_idx` (`seccion_id_seccion`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `seccion_id_seccion` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `libro`
+--
+
+INSERT INTO `libro` (`id_libro`, `nombre_libro`, `autor_libro`, `img_libro`, `url_libro`, `seccion_id_seccion`) VALUES
+(2, 'asdasasdasfascascasd', 'asdqbtymhbb  btms', '', 'uploaded/books/5605af938999b9gag.jpg', 1),
+(3, 'librito super indo', 'asdasdaszxc asdq', '', 'uploaded/books/56084f04edeff1414085283159.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -70,11 +73,77 @@ CREATE TABLE IF NOT EXISTS `libro` (
 --
 
 CREATE TABLE IF NOT EXISTS `seccion` (
-  `id_seccion` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre_seccion` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_seccion`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id_seccion` int(11) NOT NULL,
+  `nombre_seccion` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `seccion`
+--
+
+INSERT INTO `seccion` (`id_seccion`, `nombre_seccion`) VALUES
+(1, 'novela'),
+(2, 'conocimiento'),
+(3, 'ni'),
+(4, 'libro re copado'),
+(5, 'libro nuevo'),
+(6, 'ttt');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `categoria_libro`
+--
+ALTER TABLE `categoria_libro`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_fk_libro` (`id_fk_libro`),
+  ADD KEY `id_fk_categoria` (`id_fk_categoria`);
+
+--
+-- Indices de la tabla `libro`
+--
+ALTER TABLE `libro`
+  ADD PRIMARY KEY (`id_libro`),
+  ADD KEY `fk_libro_seccion_idx` (`seccion_id_seccion`);
+
+--
+-- Indices de la tabla `seccion`
+--
+ALTER TABLE `seccion`
+  ADD PRIMARY KEY (`id_seccion`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `categoria_libro`
+--
+ALTER TABLE `categoria_libro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `libro`
+--
+ALTER TABLE `libro`
+  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `seccion`
+--
+ALTER TABLE `seccion`
+  MODIFY `id_seccion` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- Restricciones para tablas volcadas
 --
--- templates/catalog.tpl
+++ templates/catalog.tpl
@@ -4,18 +4,19 @@
     <form id="uploadBook" class="uploadForm">
       <div class="form-group col-md-12">
         <div class="col-md-6">
-            <label for="exampleInputBookName">Nombre Libro</label>
-            <input type="text" class="form-control" name="bookName" id="exampleInputBookName" placeholder="Nombre">
+            <label for="InputBookName">Nombre Libro</label>
+            <input type="text" class="form-control" name="bookName" id="InputBookName" placeholder="Nombre Libro">
         </div>
       </div>
       <div class="form-group col-md-12">
         <div class="col-md-6">
-          <label for="exampleInputeDescription" >Descripcion</label>
-          <textarea class="form-control" name="bookDescription" rows="3"></textarea>
+          <label for="InputBookAuthor" >Nombre Autor</label>
+          <input type="text" class="form-control" name="bookAuthor" placeholder="Nombre Autor">
+          <!-- <textarea class="form-control" name="bookDescription" rows="3"></textarea> -->
         </div>
       </div>
       <div class="col-md-12">
-      
+
         <div class="col-md-6">
           <label for="select">Elige categoria</label>
           {if $sections}
@@ -29,13 +30,16 @@
       </div>
       <div class="col-md-12">
         <div class="form-group col-md-8">
-          <label for="bookToUpload">Agregar archivo</label>
+          <label for="bookImageToUpload">Agregar imagen portada</label>
+          <input type="file" name="bookImageToUpload">
+        </div>
+        <div class="form-group col-md-8">
+          <label for="bookToUpload">Agregar libro</label>
           <input type="file" name="bookToUpload">
         </div>
         <div class="col-md-8">
           <button data-action="add-book" type="submit" class="btn btn-default">Enviar</button>
         </div>
-
       </div>
 
     </form>
