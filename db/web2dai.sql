c90a15cbd88c29d20f9bb65c1b13e308762ef444
Leonardo Navarro <maikndawer@gmail.com>
2015-09-29 11:30:58 -0300

    se añade la tabla usuarios a la base de datos

--- db/web2dai.sql
+++ db/web2dai.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.12
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-09-2015 a las 21:46:05
--- Versión del servidor: 5.6.25
--- Versión de PHP: 5.6.11
+-- Servidor: localhost
+-- Tiempo de generación: 29-09-2015 a las 03:27:57
+-- Versión del servidor: 5.5.44-0ubuntu0.14.04.1
+-- Versión de PHP: 5.5.9-1ubuntu4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `web2dai`
@@ -27,9 +27,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `categoria` (
-  `id_categoria` int(11) NOT NULL,
-  `nombre_categoria` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_categoria` varchar(200) NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -38,10 +39,13 @@ CREATE TABLE IF NOT EXISTS `categoria` (
 --
 
 CREATE TABLE IF NOT EXISTS `categoria_libro` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_fk_libro` int(11) NOT NULL,
-  `id_fk_categoria` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_fk_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_fk_libro` (`id_fk_libro`),
+  KEY `id_fk_categoria` (`id_fk_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -50,21 +54,22 @@ CREATE TABLE IF NOT EXISTS `categoria_libro` (
 --
 
 CREATE TABLE IF NOT EXISTS `libro` (
-  `id_libro` int(11) NOT NULL,
+  `id_libro` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_libro` varchar(200) NOT NULL,
   `autor_libro` varchar(200) NOT NULL,
   `img_libro` varchar(300) NOT NULL,
   `url_libro` varchar(200) NOT NULL,
-  `seccion_id_seccion` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `seccion_id_seccion` int(11) NOT NULL,
+  PRIMARY KEY (`id_libro`),
+  KEY `fk_libro_seccion_idx` (`seccion_id_seccion`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Volcado de datos para la tabla `libro`
 --
 
 INSERT INTO `libro` (`id_libro`, `nombre_libro`, `autor_libro`, `img_libro`, `url_libro`, `seccion_id_seccion`) VALUES
-(2, 'asdasasdasfascascasd', 'asdqbtymhbb  btms', '', 'uploaded/books/5605af938999b9gag.jpg', 1),
-(3, 'librito super indo', 'asdasdaszxc asdq', '', 'uploaded/books/56084f04edeff1414085283159.jpg', 2);
+(4, 'asdasd', 'asdasd', 'uploaded/books//5609ab544684aRepresentacionDeNumerosEnteros.pdf/10 años amu.jpg', 'uploaded/books//5609ab544684aRepresentacionDeNumerosEnteros.pdf/RepresentacionDeNumerosEnteros.pdf', 3);
 
 -- --------------------------------------------------------
 
@@ -73,9 +78,10 @@ INSERT INTO `libro` (`id_libro`, `nombre_libro`, `autor_libro`, `img_libro`, `ur
 --
 
 CREATE TABLE IF NOT EXISTS `seccion` (
-  `id_seccion` int(11) NOT NULL,
-  `nombre_seccion` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `id_seccion` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_seccion` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_seccion`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Volcado de datos para la tabla `seccion`
@@ -87,63 +93,31 @@ INSERT INTO `seccion` (`id_seccion`, `nombre_seccion`) VALUES
 (3, 'ni'),
 (4, 'libro re copado'),
 (5, 'libro nuevo'),
-(6, 'ttt');
+(6, 'ttt'),
+(7, 'asd'),
+(8, 'lala');
+
+-- --------------------------------------------------------
 
 --
--- Índices para tablas volcadas
+-- Estructura de tabla para la tabla `usuarios`
 --
 
---
--- Indices de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id_categoria`);
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Indices de la tabla `categoria_libro`
---
-ALTER TABLE `categoria_libro`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_fk_libro` (`id_fk_libro`),
-  ADD KEY `id_fk_categoria` (`id_fk_categoria`);
-
---
--- Indices de la tabla `libro`
---
-ALTER TABLE `libro`
-  ADD PRIMARY KEY (`id_libro`),
-  ADD KEY `fk_libro_seccion_idx` (`seccion_id_seccion`);
-
---
--- Indices de la tabla `seccion`
---
-ALTER TABLE `seccion`
-  ADD PRIMARY KEY (`id_seccion`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
+-- Volcado de datos para la tabla `usuarios`
 --
 
---
--- AUTO_INCREMENT de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `categoria_libro`
---
-ALTER TABLE `categoria_libro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `libro`
---
-ALTER TABLE `libro`
-  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `seccion`
---
-ALTER TABLE `seccion`
-  MODIFY `id_seccion` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+INSERT INTO `usuarios` (`id_user`, `username`, `password`, `role`) VALUES
+(1, 'maikndawer', '9d0682ff6278a656e0d9cca38f47c446', 'admin');
+
 --
 -- Restricciones para tablas volcadas
 --
